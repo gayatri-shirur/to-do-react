87c787229fa1f521ad9fa2881456d11465fe4740
gayatri <gayatrimshirur@gmail.com>
2025-10-09 21:57:29 +0530

    Components and styles for Todo app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React,{useState} from 'react'
+import "./App.css"
+import TodoInput from "./components/todoinput";
+import Todolist from "./components/todolist";
 
 function App() {
-
+  const [listtodo,setListtodo]=useState([]);
+  let addList=(inputText)=>{
+    setListtodo([...listtodo,inputText]);
+    
+  }
   return (
-    <>
-
-    </>
+    <div className="main-container">
+      <div className="center-container">
+          <TodoInput addList={addList}/>
+        </div>
+         </div>
   )
 }
 
---
+++ src/components/todolist.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./Todo.css"
+
+
+function todolist(props) {
+  return (item )
+}
+
+export default todolist
