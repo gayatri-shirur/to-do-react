a15016d34ce093166b829d9a1f927928e67fe4c6
gayatri <gayatrimshirur@gmail.com>
2025-10-15 17:55:23 +0530

    practice

--- src/components/todoinput.js
+++ src/components/todoinput.js
@@ -1,18 +1,19 @@
-import React,{useState} from "react";
+
+ import React,{useState} from "react";
 
 function TodoInput(props) {
-    const [inputText, setInputText] = useState('');
+  const [inputText, setInputText] = useState('');
   return (
     <div>
-       <input type="text" 
-        className="input-box-todo" 
-        placeholder="Enter your Todo"
-        value={inputText}
-        onChange={(e)=>{
+        <input type="text" 
+         className="input-box-todo" 
+         placeholder="Enter your Todo"
+         value={inputText}
+       onChange={(e)=>{
             setInputText(e.target.value)
         }}
         />
-     <button className='add-btn'
+       <button className='add-btn'
         onClick={()=>{
             props.addList(inputText)
             setInputText("")
